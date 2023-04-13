4519123c98725c516239393d646b6baadb8d572b
Aman Gupta <aag9131@nyu.edu>
2023-04-13 12:34:23 -0700

    server changes

--- blog/app.js
+++ blog/app.js
@@ -89,6 +89,6 @@ app.get("/posts/:postID", function(req, res){
   });
 });
 
-app.listen(3000, function() {
+app.listen(process.env.PORT, function() {
   console.log("Server started on port 3000");
 });
